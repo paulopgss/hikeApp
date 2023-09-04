5392a72b8b55f894f75766935b930eb96191c128
Paulo Sousa <paulopgss@gmail.com>
2023-09-04 12:52:57 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Paulo Sousa on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
