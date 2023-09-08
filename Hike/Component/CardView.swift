c6d175d11eec265868adb5bc57bdbeeadf216d82
Paulo Sousa <paulopgss@gmail.com>
2023-09-08 13:21:24 -0300

    feat: :sparkles: structure of card

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,24 +9,67 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        // MARK: CARD
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLight")
-                        ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                )
-                .frame(width: 256, height: 256)
+            CustomBackgroundView()
             
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+            VStack {
+                // MARK: - HEADER
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Trilhas")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                        )
+                        
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("O botao foi pressionado")
+                        } label: {
+                            Text("button")
+                        }
+                    }
+                    Text("Atividade ao ar livres são divertidas e agradáveis para amigos e família.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    .customIndigoMedium,
+                                    .customSalmonLight
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK: - FOOTER
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
