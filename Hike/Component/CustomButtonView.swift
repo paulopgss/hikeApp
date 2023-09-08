d61a71bc1331d962154cf08bc85df86cfc4ee956
Paulo Sousa <paulopgss@gmail.com>
2023-09-08 13:45:32 -0300

    feat: :sparkles: create button

---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Paulo Sousa on 08/09/23.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonView()
+    }
+}
